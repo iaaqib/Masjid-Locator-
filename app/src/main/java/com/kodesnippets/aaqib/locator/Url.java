13e6211f97ea16399fa7d4f159150aa0875b0ba5
Aaqib Hussain <silent_smah@hotmail.com>
2017-01-26 11:01:51 +0500

    added request to fetch timings
    Network request added to fetch timings from Volley.

---
+++ app/src/main/java/com/kodesnippets/aaqib/locator/Url.java
@@ -0,0 +1,9 @@
+package com.kodesnippets.aaqib.locator;
+
+/**
+ * Created by silen on 1/26/2017.
+ */
+public class Url {
+
+    static String namazTimingsBaseUrl = "http://api.xhanch.com/islamic-get-prayer-time.php?loc=108410&yer=2016&mth=9&mde=json";
+}
